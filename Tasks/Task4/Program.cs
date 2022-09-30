e2165b1b9eb6ccfc66d40ff74506bf245939f74d
Kristina <kristisha_m@mail.ru>
2022-09-30 23:32:01 +0300

    add task4 in new console project

---
+++ Tasks/Task4/Program.cs
@@ -0,0 +1,76 @@
+﻿// Задача 60. ...Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
+// Массив размером 2 x 2 x 2
+// 66(0,0,0) 25(0,1,0)
+// 34(1,0,0) 41(1,1,0)
+// 27(0,0,1) 90(0,1,1)
+// 26(1,0,1) 55(1,1,1)
+
+int height = EnterInt("Введите количество столбцов массива:  ");  
+int width = EnterInt("Введите количество строк массива:  "); 
+int @long = EnterInt("Введите длину  массива: "); 
+
+int[,,] matrix = new int[height, width, @long];
+
+ CreateRandom2DArray(matrix);
+ Console.WriteLine();
+ Print2DArray(matrix);
+
+int EnterInt(string prompt)
+{
+    Console.Write(prompt);
+    return int.Parse(Console.ReadLine()!);
+}
+
+
+void CreateRandom2DArray(int[,,]matrix )
+{
+  int[] temp = new int[matrix.GetLength(0) * matrix.GetLength(1) * matrix.GetLength(2)];
+  int  number;
+  for (int i = 0; i < temp.GetLength(0); i++)
+  {
+    temp[i] = new Random().Next(10, 100);
+    number = temp[i];
+    if (i >= 1)
+    {
+      for (int j = 0; j < i; j++)
+      {
+        if (temp[i] == temp[j])
+        {
+          temp[i] = new Random().Next(10, 100);
+          j = 0;
+          number = temp[i];
+        }
+          number = temp[i];
+      }
+    }
+  }
+  int count = 0; 
+  for (int i = 0; i < matrix.GetLength(0); i++)
+  {
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+      for (int k = 0; k < matrix.GetLength(2); k++)
+      {
+        matrix[i, j, k] = temp[count];
+        count++;
+      }
+    }
+  }
+}
+
+
+void Print2DArray(int[,,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            for (int k = 0; k < matrix.GetLength(2); k++)
+            {
+                Console.Write($"{matrix[i, j, k]}({i}, {j}, {k}); ");
+            }
+            Console.WriteLine();
+        }
+        Console.WriteLine();
+    }
+}
